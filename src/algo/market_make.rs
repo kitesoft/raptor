4feaeaf79eaed5319b885b3307bb8c9f1519058e
mute1997 <mute1008@gmail.com>
2019-04-16 13:04:03 +0900

    to library

--- src/algo/market_make.rs
+++
@@ -1,9 +0,0 @@
-use crate::types::algo::Algo;
-
-pub struct MarketMake {}
-
-impl Algo for MarketMake {
-    fn ticker(&self) {
-        // TODO ロジックを実装する. 引数にmarket, executions, board, orderとかを渡す
-    }
-}
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+extern crate lazy_static;
+extern crate chrono;
+extern crate reqwest;
+#[macro_use] extern crate serde_derive;
+
+pub mod market;
+pub mod algo;
+pub mod types;
+pub mod utils;
